093f33f71555b1386c028c77ac0973cfa6bc12c2
SamridhChicmic <samridh.gupta@chicmic.co.in>
2024-09-10 16:31:30 +0530

    feat:Nakama Integrated

--- track.gd
+++ track.gd
@@ -3,10 +3,10 @@ extends Node3D
 @onready var vehicle:PackedScene = preload("res://vehicles/car_base.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(GameManager.Players)
-	for player in GameManager.Players:
-		var vehicleScene = vehicle.instantiate();
-		vehicleScene.name = str(player);
-		$Players.add_child(vehicleScene)
-		
+	print("Vichle",vehicle)
+	#for player in GameManager.Players:
+		#var vehicleScene = vehicle.instantiate();
+		#vehicleScene.name = str(player);
+		#$Players.add_child(vehicleScene)
+	pass
 	
